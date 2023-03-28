90bc0f7f80cc5e6e339002ffd219e3441e0379c9
Yoo Seungmin <dnjfht@gmail.com>
2023-03-29 07:43:49 +0900

    함수형 컴포넌트에서 useEffect() 사용하여 event listener 추가

--- src/App.js
+++ src/App.js
@@ -40,7 +40,12 @@ class App extends React.Component {
     // addEventListener(어떤 이벤트가 실행될지, 어떤 행동을 할 것인지(함수 형태))
   };
 
-  componentWillUnmount = () => {};
+  componentWillUnmount = () => {
+    this.circle.current.removeEventListener("mouseover", this.hoverEvent);
+    // 컴포넌트가 사라졌다가 생기면 event listener가 또 생기게 됨.
+    // mouseover를 한 번만 했음에도 불구하고 mouseover를 한 100번 한 것처럼 event가 계속 올라갈 수도 있다는 얘기.
+    // 이러한 현상을 막기 위하여 event listener를 등록했으면 컴포넌트가 사라질 때는 event listener를 지워줘야 함.
+  };
 
   render() {
     return (
--- src/Text.js
+++ src/Text.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 const TextH1 = styled.h1``;
 
 export default function Text() {
-  const text = React.useRef(null);
+  const text = useRef(null);
+
+  const hoverEvent = (event) => {
+    console.log(event.target);
+    console.log(text.current);
+
+    text.current.style.background = "yellow";
+  };
+
+  useEffect(() => {
+    text.current.addEventListener("mouseover", hoverEvent);
+
+    return () => {
+      text.current.removeEventListener("mouseover", hoverEvent);
+    };
+  }, [text]);
 
   return <TextH1 ref={text}>텍스트 입니다!</TextH1>;
 }
