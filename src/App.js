93a202ce5b3ed80236ecc74795af4bd3e60eb9b1
Yoo Seungmin <dnjfht@gmail.com>
2023-03-29 06:41:34 +0900

    addEventListener을 통한 event listener 추가.

--- src/App.js
+++ src/App.js
@@ -24,8 +24,20 @@ class App extends React.Component {
     this.circle = React.createRef(null);
   }
 
+  hoverEvent = (event) => {
+    console.log(event.target);
+    console.log(this.circle.current);
+    // event target과 this.circle.current가 같은지 확인해볼 거임.
+    // 같다는 걸 확인할 수 있음.
+
+    this.circle.current.style.background = "yellow";
+  };
+
   componentDidMount = () => {
     console.log(this.circle);
+    // {current: div}
+    this.circle.current.addEventListener("mouseover", this.hoverEvent);
+    // addEventListener(어떤 이벤트가 실행될지, 어떤 행동을 할 것인지(함수 형태))
   };
 
   componentWillUnmount = () => {};
